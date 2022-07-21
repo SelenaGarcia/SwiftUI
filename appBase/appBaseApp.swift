506feed0cf50e35ff76043e90529485b9d743e23
SelenaGarcia <selenagarcialobo@gmail.com>
2022-07-21 20:29:06 -0300

    Initial Commit

---
+++ appBase/appBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  appBaseApp.swift
+//  appBase
+//
+//  Created by Selena Belén Garcia Lobo on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct appBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
